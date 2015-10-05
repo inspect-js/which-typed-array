0b1ae2848372f6256cf075d687e3722878e67aca
Jordan Harband <ljharb@gmail.com>
2015-10-05 15:14:02 -0700

    Implementation

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var forEach = require('foreach');
+var bind = require('function-bind');
+
+var toStr = bind.call(Function.call, Object.prototype.toString);
+var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
+
+var typedArrays = {
+	Float32Array: true,
+	Float64Array: true,
+	Int8Array: true,
+	Int16Array: true,
+	Int32Array: true,
+	Uint8Array: true,
+	Uint8ClampedArray: true,
+	Uint16Array: true,
+	Uint32Array: true
+};
+
+var slice = bind.call(Function.call, String.prototype.slice);
+var toStrTags = {};
+var gOPD = Object.getOwnPropertyDescriptor;
+if (hasToStringTag && gOPD && Object.getPrototypeOf) {
+	forEach(typedArrays, function (_, typedArray) {
+		var arr = new global[typedArray]();
+		var proto = Object.getPrototypeOf(arr);
+		toStrTags[typedArray] = gOPD(proto, Symbol.toStringTag).get;
+	});
+}
+
+var tryTypedArrays = function tryTypedArrays(value) {
+	var foundName = false;
+	forEach(toStrTags, function (getter, typedArray) {
+		if (!foundName) {
+			try {
+				var name = getter.call(value);
+				if (name === typedArray) {
+					foundName = name;
+				}
+			} catch (e) {/**/}
+		}
+	});
+	return foundName;
+};
+
+var isTypedArray = require('is-typed-array');
+
+module.exports = function whichTypedArray(value) {
+	if (!isTypedArray(value)) { return false; }
+	if (!hasToStringTag) { return slice(toStr(value), 8, -1); }
+	return tryTypedArrays(value);
+};
